84246e5dc7b788f3ee9b1cbc26d43527943c5f40
breakingmews <breakingmews101@gmail.com>
2024-10-18 17:06:30 +0200

    Parse torrent file

--- app/main.py
+++ app/main.py
@@ -1,37 +1,38 @@
 import json
 import sys
 import bencodepy
-from bencode import bencode
-
-# import bencodepy - available if you need it!
-# import requests - available if you need it!
 
 
-def decode_bencode(bencoded_value):
-    bc = bencodepy.Bencode(encoding='utf-8')
+def decode_value(bencoded_value):
+    bc = bencodepy.Bencode(encoding="utf-8")
     return bc.decode(bencoded_value)
 
+
+def decode_file(filepath: str):
+    bc = bencodepy.Bencode()
+    content = bc.read(filepath)
+    # print(content)
+    return content
 
 
 def main():
     command = sys.argv[1]
+    commands = ["decode", "info"]
+    if not(command in commands):
+        raise NotImplementedError(f"Unknown command {command}")
 
     if command == "decode":
-        bencoded_value = sys.argv[2].encode()
+        bencoded_value = sys.argv[2]
+        decoded: str = decode_value(bencoded_value)
+        print(json.dumps(decoded))
+    elif command == "info":
+        filepath =  sys.argv[2]
+        # print(read_file(filepath))
+        decoded: dict = decode_file(filepath)
+        print(f"Tracker URL: {decoded[b"announce"].decode()}")
+        print(f"Length: {decoded[b"info"][b"length"]}")
 
-        # json.dumps() can't handle bytes, but bencoded "strings" need to be
-        # bytestrings since they might contain non utf-8 characters.
-        #
-        # Let's convert them to strings for printing to the console.
-        def bytes_to_str(data):
-            if isinstance(data, bytes):
-                return data.decode()
 
-            raise TypeError(f"Type not serializable: {type(data)}")
-
-        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
-    else:
-        raise NotImplementedError(f"Unknown command {command}")
 
 
 if __name__ == "__main__":
--- test/test_main.py
+++ test/test_main.py
@@ -1,32 +1,41 @@
 import unittest
-from app.main import decode_bencode
+from app.main import decode_value
 
 
 class TestMain(unittest.TestCase):
     def test_decode_bencode_str_1(self):
-        decoded = decode_bencode(b"5:hello")
+        decoded = decode_value(b"5:hello")
         expected = "hello"
         self.assertEqual(expected, decoded)
 
     def test_decode_bencode_str_2(self):
-        decoded = decode_bencode(b"10:hello12345")
+        decoded = decode_value(b"10:hello12345")
         expected = "hello12345"
         self.assertEqual(expected, decoded)
 
     def test_decode_bencode_int(self):
-        decoded = decode_bencode(b"i52e")
+        decoded = decode_value(b"i52e")
         expected = 52
         self.assertEqual(expected, decoded)
 
     def test_decode_bencode_list(self):
-        decoded = decode_bencode(b"l5:helloi52ee")
+        decoded = decode_value(b"l5:helloi52ee")
         expected = ["hello", 52]
         self.assertEqual(expected, decoded)
 
     def test_decode_bencode_dict(self):
-        decoded = decode_bencode(b"d3:foo3:bar5:helloi52ee")
+        decoded = decode_value(b"d3:foo3:bar5:helloi52ee")
         expected = {"foo":"bar","hello":52}
         self.assertEqual(expected, decoded)
+
+    @unittest.skip("Test file generator")
+    def test1(self):
+        content = b"d8:announce55:http://bittorrent-test-tracker.codecrafters.io/announce10:created by13:mktorrent 1.14:infod6:lengthi2994120e4:name12:codercat.gif12:piece lengthi262144e6:pieces240:<40\x9f\xae\xbf\x01\xe4\x9c\x0fc\xc9\x0b~\xdc\xc2%\x9bj\xd0\xb8Q\x9b.\xa9\xbb7?\xf5g\xf6DB\x81V\xc9\x8a\x1d\x00\xfc\x9d\xc8\x13fXu6\xf4\x8c \x98\xa1\xd7\x96\x92\xf2Y\x0f\xd9\xa6\x03<a\xe7\x17\xf8\xc0\xd1\xe5XPh\x0e\xb4Q\xe3T;b\x03oT\xe7F\xec6\x9fe\xf3-E\xf7{\x1f\x1c7b\x1f\xb9e\xc6VpKx\x10~\xd5S\xbd\x08\x13\xf9/\xefx\x02g\xc0{t1\xb8h17\xd2\x0f\xf5\x94\xb1\xf1\xbf?\x885\x16]h\xfb\x042\xbd\x8ew\x96\x08\xd2w\x82\xb7y\xc7s\x80b\xe9\xb5\n\xb5\xd6\xbc\x04\t\xa0\xf3\xa9P8Wf\x9dG\xfeu-Ew\xea\x00\xa8n\xe6\xab\xbc0\xcd\xdb\x80\n\x0bb\xd7\xa2\x96\x11\x11f\xd89x?R\xb7\x0f\x0c\x90-V\x19k\xd3\xee\x7f7\x9b]\xb5~;=\x8d\xb9\xe3M\xb6;K\xa1\xbe'\x93\t\x11\xaa7\xb3\xf9\x97\xddee"
+        import bencodepy
+        with open("test1.torrent", "wb") as f:
+            bencodepy.bwrite(content, f)
+
+
 
 
 if __name__ == '__main__':
